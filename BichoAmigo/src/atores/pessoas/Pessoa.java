1130142517c4880a52180603f5d22f34f06718bb
0337188 <0337188@200.132.53.28>
2020-03-10 10:26:10 -0300

    Commit com uma classe "Pessoa" criada

---
+++ BichoAmigo/src/atores/pessoas/Pessoa.java
@@ -0,0 +1,39 @@
+package atores.pessoas;
+/**
+ * @author 0337188
+ */
+public class Pessoa {
+    
+    // atributos
+    private String nome;
+    private String cpf;
+    private String telefone;
+    //private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    
+    
+}
